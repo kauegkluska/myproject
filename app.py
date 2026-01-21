7bd6c550269c66b526a229c511ec466c7064894c
Kauê Kluska <132571134+kauegkluska@users.noreply.github.com>
2026-01-20 21:05:13 -0300

    Configure app to run on dynamic port
    Set the app to run on a configurable port from environment variables.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from app import create_app, db
+import os
 
 app = create_app()
 app.secret_key = "sua_chave_secreta_aqui"
@@ -8,3 +9,5 @@ with app.app_context():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # usa a porta do Render ou 5000 local
+    app.run(host="0.0.0.0", port=port, debug=True)
